e997c6feaca22bac846d182208dfd966286da6c6
suntao039 <729631217@qq.com>
2018-04-03 19:24:09 +0800

    新增ejs功能

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const Koa = require('koa')
 const app = new Koa()
-const {normal} = require('./tpl')
-
+const {htmlTpl,ejsTpl} = require('./tpl')
+const ejs = require('ejs')
 
 app.use(async (ctx,next)=>{
-    ctx.type = 'text/html; charset="UTF-8"'
-    ctx.body =normal
+    ctx.type = 'text/html; charset=UTF-8'
+    ctx.body =ejs.render(ejsTpl,{
+        you :'lucke',
+        me :'hello'
+    })
 })
 app.listen(4455)
--- server/tpl/index.js
+++ server/tpl/index.js
@@ -1,5 +1,4 @@
-const normalTpl =require('./nomal')
-
 module.exports = {
-    normal: normalTpl
+    htmlTpl: require('./html'),
+    ejsTpl  : require('./ejs')
 }
