98416be7fffd955ffd5b93daa249f3aa7a84e943
suntao039 <729631217@qq.com>
2018-04-03 15:00:45 +0800

    第二次提交，但样式还是有问题，需要看老师的源码

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
+const {normal} = require('./tpl')
+
+
 app.use(async (ctx,next)=>{
-    ctx.body ='电影首页'
+    ctx.type = 'text/html; charset="UTF-8"'
+    ctx.body =normal
 })
 app.listen(4455)
