7d908c47f59b257a6a529d3601be6c2c9d0f176c
suntao039 <729631217@qq.com>
2018-04-04 12:03:08 +0800

    pug模板首页展示

--- server/tpl/pug.js
+++ server/tpl/pug.js
@@ -1,28 +1,21 @@
 module.exports =`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name = 'viewport' content="width=device-width,initial-scale=1">
-    <title>koa server Html</title>
-    <link herf = "https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-    <script src="./jquery.min.js"></script>
+doctype html
+html
+ head
+  meta(charset="UTF-8")
+  meta(name = 'viewport' content="width=device-width,initial-scale=1")
+   title koa server Pug
+   link(herf = "https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet")
+   script(src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js")
+   script(src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js") 
     
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <h1>hi hello</h1>
-                <p>this is my home</p>          
-            </div>
-            <div class="col-md-4">
-                <p>测试静态文件</p>     
-            </div>                 
-        </div>                  
-    </div>
-</body>
-</html>
-    
+
+ body
+  .container
+    .row
+      .col-md-8
+        h1 hi #{you}
+        p this is #{me}
+      .col-md-4
+        p 测试静态文件 pug 模板引擎       
 `
